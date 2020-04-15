b7bae0c402756519a276e51b7b35e24e9698ad4b
Jeanne Choo <jchoo@aisingapore.org>
2020-04-15 19:13:44 +0800

    added setup.py to make repo pip installable

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='Golden Retriever',
+      version='0.1',
+      description='Information retrieval using fine-tuned semantic similarity',
+      author='AI Singapore',
+      packages=find_packages(),
+      zip_safe=False)
