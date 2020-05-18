f4e2c79dfe0f5cecad111385413d45a11061cb6a
vinitrinh <jeremy_tan@kelaberetiv.sg>
2020-05-18 15:41:45 +0800

    refactored finetune scripts

---
+++ src/finetune/config.py
@@ -0,0 +1,39 @@
+config_dict={
+    "beta_1": 0.9, 
+    "beta_2": 0.999, 
+    "epsilon": 1e-07, 
+    "learning_rate": 5e-05, 
+    "early_stopping_steps": 5,
+    "margin": 0.3, 
+    "loss_type": "triplet", # "cosine"
+    "num_epochs": 1, 
+
+    "task_type": "train_eval", 
+    "train_batch_size": 1,
+    "predict_batch_size": 2,
+
+    "save_dir": "./test_finetune", 
+    "eval_model_dir": None, 
+    "model_name": "USE", # "USE", "albert", "bert" 
+    "max_seq_length": 256, 
+    
+    "kb_names": ["qa_nrf"], 
+    "random_seed": 42, 
+
+    "MINIO_URL":"localhost:9001",
+    "MINIO_ACCESS_KEY":"minio",
+    "MINIO_SECRET_KEY":"minio123",
+    "SAVEDMODELNAME":"finetuned"
+    }
+
+class config_obj:
+    
+    def __init__(self, config_dict=config_dict):
+        self.__dict__.update(config_dict)
+        self.config_dict = config_dict
+
+    def __str__(self):
+        return str(self.config_dict)
+    
+CONFIG=config_obj()
+            
